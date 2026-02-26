b136deed93e72e1c79d2a80484f74fe256f297ad
romaji <romaji@NEOFORM>
2026-02-26 15:37:04 -0600

    Add project files.

---
+++ EpsilonCombatEngine/NeverIsNotATimeException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpsilonCombatEngine
+{
+	/// <summary>
+	/// Exception raised when trying to read the time value of a TimeOrNever when it is currently Never
+	/// </summary>
+	[Serializable]
+	public class NeverIsNotATimeException : Exception
+	{
+		public NeverIsNotATimeException(): base() { }
+		public NeverIsNotATimeException(string message) : base(message) { }
+		public NeverIsNotATimeException(string message, Exception inner) : base(message, inner) { }
+	}
+}
---
+++ TestMachine/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestMachine
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
