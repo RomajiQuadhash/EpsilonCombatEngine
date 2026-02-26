a62888ffab1086317f0d57614a19064f1f96d175
romaji <romaji@NEOFORM>
2026-02-26 15:37:04 -0600

    Add project files.

---
+++ EpsilonCombatEngine/Counter.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace EpsilonCombatEngine
+{
+	/// <summary>
+	/// This class keeps track of a value that may change over time.
+	/// A counter can also tell how long until it passes a threshold or how far in the past that threshold was crossed.
+	/// This only works if the counter is linear over that time, however!
+	/// </summary>
+	/// <typeparam name="T">Number type to use</typeparam>
+	public class Counter<T> where T : INumber<T>
+    {
+
+    }
+}
---
+++ EpsilonCombatEngine/TimeOrNever.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpsilonCombatEngine
+{
+	/// <summary>
+	/// Used to represent when something may happen or if it won't ever happen
+	/// </summary>
+	/// <typeparam name="T">Format of time to use</typeparam>
+	internal class TimeOrNever<T> where T : INumber<T>
+	{
+		public TimeOrNever() => _internalTime = T.Zero;
+
+		public TimeOrNever(T t) => _internalTime = t;
+
+		/// <summary>
+		/// Set to true to mark this as invalid
+		/// </summary>
+		public bool IsNever;
+
+		private T _internalTime;
+		/// <summary>
+		/// Gets the time if valid. If set, clears is never
+		/// </summary>
+		public T Time { 
+			get
+			{
+				if (IsNever)
+				{
+					throw new NeverIsNotATimeException("Can't get the time, the time is Never!");
+				}
+				return _internalTime;
+			}
+			set 
+			{
+				_internalTime = value;
+				IsNever = false;
+			}
+		}
+	}
+}
