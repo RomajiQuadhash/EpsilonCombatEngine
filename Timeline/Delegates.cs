2210086e63a8543ef21c5ad6bb921ea6802e1bae
romaji <nxt101@gmail.com>
2026-03-11 12:15:15 -0500

    Create Timeline project, IOkazo
    Events used by this engine all implement IOkazo. This is so the Timeline
    can treat them all the same when constructed.

---
+++ Timeline/Delegates.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+/// <summary>
+/// Common delegates used by the timeline, such as for advancing time or communicating that an event occured.
+/// </summary>
+namespace Timeline
+{
+    /// <summary>
+    /// Event that is raised when time advances on the timeline, allowing listeners to update their state based on the amount of time that has passed.
+    /// </summary>
+    /// <typeparam name="T">The numeric type used for the timeline</typeparam>
+    /// <param name="sender">Always the current timeline object</param>
+    /// <param name="timeElapsed">How much time passes. Negative if an event is overshot for some reason.</param>
+    public delegate void Advance<T>(object sender, T timeElapsed) where T : INumber<T>;
+    /// <summary>
+    /// Event that is raised when an event occurs on the timeline, allowing listeners to react to the event and update their state accordingly.
+    /// </summary>
+    /// <typeparam name="T">The numeric type used for the timeline</typeparam>
+    /// <param name="sender">The event that is occuring. Should be of type IOkazo</param>
+    /// <param name="okazo">Same as the sender, just typed</param>
+    public delegate void Occur<T>(object sender,IOkazo<T> okazo) where T : INumber<T>;
+}
---
+++ Timeline/IOkazo.cs
@@ -0,0 +1,69 @@
+﻿using CoreMetrics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timeline
+{
+    /// <summary>
+    /// Used as an event, but in Esperato, since "event" is a reserved word.
+    /// Comparison must sort by time remaining first, then by some other factor to ensure a deterministic order of events that occur at the same time. This is important to ensure that the timeline behaves predictably and that events are processed in a consistent order, even when they have the same time remaining.
+    /// </summary>
+    /// <typeparam name="T">The numeric type used for the timeline</typeparam>
+    public interface IOkazo<T> : IComparable<IOkazo<T>> where T : INumber<T>
+    {
+        /// <summary>
+        /// Amount of time until this event occurs. If this event is dynamic, this value may change based on other factors besides time advancing.
+        /// If negative, time will be rewound to bring this to zero before the event occurs, ensuring that all timeline dependent factors show correctly while the event occurs, and any new events created by this event will be scheduled correctly relative to this event.
+        /// If this is Never, there is no time associated with the event currently and it should be saved separately or removed.
+        /// </summary>
+        public TimeOrNever<T> TimeRemaining { get; }
+        /// <summary>
+        /// If two events have the same time remaining, this method is used to determine which one should occur first.
+        /// </summary>
+        /// <param name="other">The other event</param>
+        /// <returns>-1 if this event should take place before the other, +1 if after. Only zero if the other is this</returns>
+        internal int Tiebreaker(IOkazo<T> other);
+
+        /// <summary>
+        /// Compares the current instance with another IOkazo<T> object and returns an integer that indicates their
+        /// relative order based on time remaining and a tiebreaker.
+        /// </summary>
+        /// <remarks>The comparison first considers the TimeRemaining property. If the values are equal, a
+        /// tiebreaker is used to determine the order. This is useful to sort what happens on the timeline.
+        /// </remarks>
+        /// <param name="other">The IOkazo<T> instance to compare with the current object. Cannot be null.</param>
+        /// <returns>A value less than zero if the current instance should occur before the other; zero if they are equal;
+        /// or greater than zero if the current instance should occur after.</returns>
+        public new int CompareTo(IOkazo<T> other)
+        {
+            int timeComparison = TimeRemaining.CompareTo(other.TimeRemaining);
+            if (timeComparison != 0)
+            {
+                return timeComparison;
+            }
+            return Tiebreaker(other);
+        }
+        /// <summary>
+        /// Handles the advancement event by processing the elapsed time since the last update.
+        /// </summary>
+        /// <param name="sender">The Timeline advancing time</param>
+        /// <param name="timeElapsed">The amount of time to move the event by.</param>
+        public void OnAdvance(object sender, T timeElapsed);
+
+        /// <summary>
+        /// Raised when this event occurs, allowing listeners to react to the event and update their state accordingly.
+        /// </summary>
+        public event Occur<T>? Occuring;
+
+        /// <summary>
+        /// Executes the event, rewinding time if necessary, and raises the Occuring event to notify listeners that the event has occurred and take other actions. 
+        /// After this method is called, the event should be considered completed and should be removed from the timeline.
+        /// Note that if this is a dynamic event, this might represent a state change, rather than an action to be taken so it might do nothing besides informing listeners.
+        /// </summary>
+        public void Trigger();
+    }
+}
