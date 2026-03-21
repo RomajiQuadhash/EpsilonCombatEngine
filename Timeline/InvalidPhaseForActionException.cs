596721f8a8a9a840a95e2c07ef6038f568b7ba16
romaji <nxt101@gmail.com>
2026-03-20 19:21:46 -0500

    Skeleton for the Timeline!
    There's a lot of stuff here that doesn't work yet but the concepts exist
    Hopefully this means I can actually do this later.

---
+++ Timeline/InvalidPhaseForActionException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timeline
+{
+    /// <summary>
+    /// Raised by Timeline when an action is attempted that is not allowed in the current phase.
+    /// Examples include adding or adjusting events when not in the Open phase, or calling Update when in the Terminated phase.
+    /// </summary>
+    [Serializable]
+    public class InvalidPhaseForActionException: Exception
+    {
+        public InvalidPhaseForActionException() : base() { }
+        public InvalidPhaseForActionException(string message) : base(message) { }
+        public InvalidPhaseForActionException(string message, Exception inner) : base(message, inner) { }
+    }
+}
