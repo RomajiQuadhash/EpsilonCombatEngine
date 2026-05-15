35ddfc944f5db740450ea179f580a0fad7018128
romaji <nxt101@gmail.com>
2026-05-14 21:01:00 -0500

    Handle threshold transition tiebreakers
    Effectively, the idea is to prioritize the one that would have happened
    "first" in the same instant, by seeing how far they passed the target.

---
+++ Timeline/OkazoOptionalProperties/IOkTransition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timeline.OkazoOptionalProperties
+{
+    /// <summary>
+    /// For events triggered by crossing a threshold.
+    /// See the TransitionData class for more information on the data used in comparison.
+    /// </summary>
+    internal interface IOkTransition<T> where T : INumber<T>
+    {
+        public TransitionData<T> TransitionData { get; }
+    }
+}
