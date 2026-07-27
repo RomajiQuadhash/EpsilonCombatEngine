a2e303887cf522721559e7298b94a5df3ac8f9c0
romaji <nxt101@gmail.com>
2026-07-27 09:32:21 -0500

    Create TimelineTests for all but TakeSteps
    TakeSteps is more complex, using specific events and sorting to tell
    what should happen next.

---
+++ Timeline-Test/TimelineTakeStepsTests.cs
@@ -0,0 +1,15 @@
+﻿using CoreMetrics;
+using System.Numerics;
+using Timeline;
+using System.Threading.Tasks;
+
+namespace Timeline_Test
+{
+    /// <summary>
+    /// All tests related to the TakeSteps method of the Timeline class.
+    /// This assumes that all tests in TimelineBasicTests have passed, since TakeSteps needs events and phases to work properly.
+    /// </summary>
+    public class TimelineTakeStepsTests
+    {
+    }
+}
