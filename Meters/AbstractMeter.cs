d56c82155556b616bdfd1e443f28fdd9580eaeaf
romaji <nxt101@gmail.com>
2026-08-06 15:51:42 -0500

    Add Meters.csproj (abstract)
    Set up what I expect will be the patterns for the Meters Project
    will be and a TODO.txt of what I'd be implementing after.

---
+++ Meters/AbstractMeter.cs
@@ -0,0 +1,81 @@
+﻿using CoreMetrics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Timeline;
+
+namespace Meters
+{
+    /// <summary>
+    /// An abstract placeholder for meters that track a numeric value over time.
+    /// Examples might be health, mana, or a vibe meter.
+    /// Unlike a Counter, we don't need the time and metric types to be the same.
+    /// </summary>
+    /// <typeparam name="T">The numeric type used for the timeline</typeparam>
+    /// <typeparam name="M">The numeric type used for the metric</typeparam>
+    public abstract class AbstractMeter<T, M> : IAdvanceable<T> where T : INumber<T> where M : INumber<M>
+    {
+        /// <summary>
+        /// The timeline this meter belongs to. Should NEVER change, since a timeline should exist for the entire lifetime of a battle,
+        /// and all meters should be destroyed when the battle ends.
+        /// TODO: see if I can make this set once only.
+        /// </summary>
+        public required Timeline<T> OwningTimeline { get; set; }
+        /// <summary>
+        /// Note that there might be sub-details if say, M is a discrete type.
+        /// You should use this for calculations though.
+        /// </summary>
+        public abstract M CurrentValue { get; }
+        /// <summary>
+        /// The rate of change of the meter's value, possibly needing to be divided by DerivativeDenominator to get the actual rate of change.
+        /// Note that if we have more complex meters, advancing time might not just add this to the current value.
+        /// But no matter what, a subclass should set this so we can use it for ThresholdEvents
+        /// </summary>
+        public abstract M Derivative { get; }
+        /// <summary>
+        /// If M is a discrete type, this is the value that Derivative is divided by to get the actual rate of change.
+        /// If M is a continuous type, this should be 1, since we can just divide the two.
+        /// </summary>
+        public abstract T DerivativeDenominator { get; }
+        /// <summary>
+        /// What the most recent instant change to the meter was. Set to 0 after each advance, since it is only relevant for transitions,
+        /// and keeping it around would lead to incorrect data about the most recent transition.
+        /// </summary>
+        public abstract M InstantChange { get; }
+        //We need to know the min and max values of the meter, since it is needed for threshold events and to know if the meter is at a limit.
+        public abstract M MinValue { get; }
+        public abstract M MaxValue { get; }
+        /// <summary>
+        /// The range is more useful for making transition data, so we provide it here for convenience. 
+        /// It also is NOT virtual, since increasing the range should be done by changing the min and max values to set how it should expand.
+        /// </summary>
+        public M Range => MaxValue - MinValue;
+        public abstract void OnAdvance(object? sender, T timeElapsed); //For a simple meter, this is really simple.
+        //But, I'm not going to bake that in at this stage, given I'm not coding anything at this stage
+
+        /// <summary>
+        /// Returns, given current properties, the time at which the meter will touch the specified threshold.
+        /// Note that this is a prediction based on current properties.
+        /// If M is a discrete type, this is the earliest time at which the meter will be equal to the threshold.
+        /// </summary>
+        /// <param name="threshold">The value to see how long it will take to reach</param>
+        /// <param name="allowNegative">If true, will return the earliest even if it is in the past. If false, if the only transition(s) are in the past, will return Never. Otherwise, the first one at or equal to time 0</param>
+        /// <returns>The time at which the meter will touch the threshold or Never if it won't. 0 if already there</returns>
+        public abstract TimeOrNever<T> ThresholdTouch(M threshold,bool allowNegative);
+        /// <summary>
+        /// Almost the same as ThresholdTouch, but with a few differences:
+        /// 1. If the meter isn't moving, it will return Never instead of 0 if the meter is already at the threshold.
+        /// 2. If M is a discrete type, this is the latest time at which the meter will be equal to the threshold, rather than the earliest.
+        /// </summary>
+        /// <param name="threshold">The value to see how long it will take to pass</param>
+        /// <param name="allowNegative">If true, will return the earliest even if it is in the past. If false, if the only transition(s) are in the past, will return Never. Otherwise, the first one at or equal to time 0</param>
+        /// <returns>The time at which the meter will cross the threshold or Never if it won't. 0 if already there and the threshold is moving</returns>
+        public abstract TimeOrNever<T> ThresholdCross(M threshold, bool allowNegative);
+
+        //Note that there's no setters because that is specific to what the meter is holding and how it works.
+        //These properties are for AbstractThresholdEvent to work with any kind of meter
+    }
+}
---
+++ Meters/AbstractThresholdEvent.cs
@@ -0,0 +1,59 @@
+﻿using CoreMetrics;
+using System.Numerics;
+using Timeline;
+using Timeline.OkazoOptionalProperties;
+
+namespace Meters
+{
+    /// <summary>
+    /// An abstract placeholder for events that are triggered by crossing a threshold.
+    /// Examples might be hitting the top or bottom of the range (like a KO, death, or max health/MP)
+    /// or crossing a specific threshold (like getting a vibe meter to sufficently rage/focus to perform an action).
+    /// </summary>
+    /// <typeparam name="T">The numeric type used for the timeline</typeparam>
+    /// <typeparam name="M">The numeric type used for the metric</typeparam>
+    public abstract class AbstractThresholdEvent<T,M> : Okazo<T>, IOkUUID, IOkPriority,IOkTransition<M> where T : INumber<T> where M : INumber<M>
+    {
+        /// <summary>
+        /// Guarentee that there's at least one difference between two events
+        /// UNLESS YOU'RE A UNIT TEST, NEVER SET THIS
+        /// </summary>
+        public int UUID { get; set; } = Guid.NewGuid().GetHashCode();
+        /// <summary>
+        /// Used to get the transition data and compute the time remaining until the threshold is crossed.
+        /// </summary>
+        public abstract AbstractMeter<T,M> OwningMeter { get; }
+        //Almost any priority could happen when a threshold is crossed, not just the ones that should always be from a threshold crossing.
+        //So, no need to restrict the priority to a specific set of values, just use the general PrioityRank enum.
+        public abstract PrioityRank? Priority { get; }
+        /// <summary>
+        /// Note that if this event is never, this will throw an InvalidOperationException,
+        /// since we'll construct a TransitionData object with invalid data.
+        /// This is OK because a never event will never end up in OkazoComparitor.Compare
+        /// </summary>
+        /// <remarks>TODO: consider if we can just, implement this</remarks>
+        public abstract TransitionData<M> TransitionData { get; }
+        /// <summary>
+        /// Explicitly settable, since we might want to disable an event without removing it immediately
+        /// (it will be removed when the timline is cleaned (Open and PostEffect when TakeSteps is called)
+        /// </summary>
+        public override bool CouldOccur { get; set; }
+        /// <summary>
+        /// Gets the remaining time until the threshold event occurs, or a value indicating it will "never" occur.
+        /// "Never" really means "not predicted to occur based on current properties", 
+        /// since the meter might change in the future and make it possible for the threshold to be crossed.
+        /// </summary>
+        /// <remarks>The value is calculated by the ComputeTimeRemaining method. 
+        /// Setting this property is illogical and will throw an InvalidOperationException.</remarks>
+        public override TimeOrNever<T> TimeRemaining { get {return ComputeTimeRemaining(); } set { throw new InvalidOperationException(); } }
+        /// <summary>
+        /// Used to compute the remaining time until the threshold event occurs.
+        /// </summary>
+        /// <returns>The remaining time until the threshold event occurs, or Never if it isn't predicted to occur</returns>
+        /// <remarks>Why not just call OwningMeter.ThresholdTouch or OwningMeter.ThresholdCross?
+        /// Well, which one is correct might depend of what the purpose of the threshold is, but there's another difficulty.
+        /// We might want to call them with allowNegative=true sometimes, but usually with it false.
+        /// </remarks>
+        internal abstract TimeOrNever<T> ComputeTimeRemaining();
+    }
+}
